59fb6c6f1efba5973c0dadac49e72e97bc5794dd
notSure-ded <89379064+notSure-ded@users.noreply.github.com>
2023-01-27 15:55:44 +0530

    Add files via upload

---
+++ nonveg.js
@@ -0,0 +1,49 @@
+let count = 0;
+document.getElementById("inc").addEventListener("click", function() {
+  count++;
+  document.getElementById("count").innerHTML = count;
+});
+document.getElementById("dec").addEventListener("click", function() {
+  count--;
+  document.getElementById("count").innerHTML = count;
+});
+
+let count2 = 0;
+document.getElementById("inc2").addEventListener("click", function() {
+  count2++;
+  document.getElementById("count2").innerHTML = count2;
+});
+document.getElementById("dec2").addEventListener("click", function() {
+  count2--;
+  document.getElementById("count2").innerHTML = count2;
+});
+
+let count3 = 0;
+document.getElementById("inc3").addEventListener("click", function() {
+  count3++;
+  document.getElementById("count3").innerHTML = count3;
+});
+document.getElementById("dec3").addEventListener("click", function() {
+  count3--;
+  document.getElementById("count3").innerHTML = count3;
+});
+
+let count4= 0;
+document.getElementById("inc4").addEventListener("click", function() {
+  count4++;
+  document.getElementById("count4").innerHTML = count4;
+});
+document.getElementById("dec4").addEventListener("click", function() {
+  count4--;
+  document.getElementById("count4").innerHTML = count4;
+});
+
+let count5 = 0;
+document.getElementById("inc5").addEventListener("click", function() {
+  count5++;
+  document.getElementById("count5").innerHTML = count5;
+});
+document.getElementById("dec5").addEventListener("click", function() {
+  count5--;
+  document.getElementById("count5").innerHTML = count5;
+});
---
+++ veg.js
@@ -0,0 +1,69 @@
+let count = 0;
+document.getElementById("inc").addEventListener("click", function() {
+  count++;
+  document.getElementById("count").innerHTML = count;
+});
+document.getElementById("dec").addEventListener("click", function() {
+  count--;
+  document.getElementById("count").innerHTML = count;
+});
+
+let count2 = 0;
+document.getElementById("inc2").addEventListener("click", function() {
+  count2++;
+  document.getElementById("count2").innerHTML = count2;
+});
+document.getElementById("dec2").addEventListener("click", function() {
+  count2--;
+  document.getElementById("count2").innerHTML = count2;
+});
+
+let count3 = 0;
+document.getElementById("inc3").addEventListener("click", function() {
+  count3++;
+  document.getElementById("count3").innerHTML = count3;
+});
+document.getElementById("dec3").addEventListener("click", function() {
+  count3--;
+  document.getElementById("count3").innerHTML = count3;
+});
+
+let count4= 0;
+document.getElementById("inc4").addEventListener("click", function() {
+  count4++;
+  document.getElementById("count4").innerHTML = count4;
+});
+document.getElementById("dec4").addEventListener("click", function() {
+  count4--;
+  document.getElementById("count4").innerHTML = count4;
+});
+
+let count5 = 0;
+document.getElementById("inc5").addEventListener("click", function() {
+  count5++;
+  document.getElementById("count5").innerHTML = count5;
+});
+document.getElementById("dec5").addEventListener("click", function() {
+  count5--;
+  document.getElementById("count5").innerHTML = count5;
+});
+
+let count6 = 0;
+document.getElementById("inc6").addEventListener("click", function() {
+  count6++;
+  document.getElementById("count6").innerHTML = count6;
+});
+document.getElementById("dec6").addEventListener("click", function() {
+  count6--;
+  document.getElementById("count6").innerHTML = count6;
+});
+
+let count7 = 0;
+document.getElementById("inc7").addEventListener("click", function() {
+  count7++;
+  document.getElementById("count7").innerHTML = count7;
+});
+document.getElementById("dec7").addEventListener("click", function() {
+  count7--;
+  document.getElementById("count7").innerHTML = count7;
+});
